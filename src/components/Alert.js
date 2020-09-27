13ae71fe76cfc3bbc1cd04d55c9aa9b4b138d264
Sapphire <info-sapphire@yandex.ru>
2020-09-27 14:07:50 +0300

    feat(Alert): add transition

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,19 +1,26 @@
 import React, { useContext } from "react";
+import { CSSTransition } from "react-transition-group";
 import { AlertContext } from "../context/alert/AlertContext";
 
 export const Alert = () => {
   const { alert, hide } = useContext(AlertContext);
 
-  if (!alert.visible) {
-    return null;
-  }
-
   return (
-    <div className={`alert alert-${alert.type || "warning"} alert-dismissible`}>
-      <strong>Attention!</strong>&nbsp;{alert.text}
-      <button onClick={hide} type="button" className="close">
-        &times;
-      </button>
-    </div>
+    <CSSTransition
+      in={alert.visible}
+      timeout={{ enter: 500, exit: 350 }}
+      classNames={"alert"}
+      mountOnEnter
+      unmountOnExit
+    >
+      <div
+        className={`alert alert-${alert.type || "warning"} alert-dismissible`}
+      >
+        <strong>Attention!</strong>&nbsp;{alert.text}
+        <button onClick={hide} type="button" className="close">
+          &times;
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
