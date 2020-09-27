7e6c1e95f738d2e560c62e2ee6065d6f2c85ec72
Sapphire <info-sapphire@yandex.ru>
2020-09-27 13:59:27 +0300

    feat: add removeNote

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AlertContext } from "../context/alert/AlertContext";
+import { FirebaseContext } from "../context/firebase/FirebaseContext";
 
 export const Notes = ({ notes }) => {
+  const alert = useContext(AlertContext);
+  const { removeNote } = useContext(FirebaseContext);
+
+  const onRemove = (noteId) => {
+    removeNote(noteId)
+      .then(() => {
+        alert.show("Note successful removed!", "success");
+      })
+      .catch(() => {
+        alert.show("Something went wrong", "danger");
+      });
+  };
+
   return (
     <ul className="list-group">
       {notes.map((note) => (
@@ -9,7 +24,11 @@ export const Notes = ({ notes }) => {
             <strong>{note.title}</strong>
             <small>{note.date}</small>
           </div>
-          <button type="button" className="btn btn-outline-danger btn-sm">
+          <button
+            type="button"
+            className="btn btn-outline-danger btn-sm"
+            onClick={() => onRemove(note.id)}
+          >
             &times;
           </button>
         </li>
